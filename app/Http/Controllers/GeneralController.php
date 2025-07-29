e25fad0bfcea879d21bad7b681c69009aca39e23
Nelson Serrano Herrera <nserranoherrera@gmail.com>
2025-07-29 16:06:22 -0400

    filtro estamento cuenta_usuario = 1

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -72,7 +72,7 @@ class GeneralController extends Controller
     public function getInfoInicial(Request $request){
         return [
             'servicios' => GenServicio::where('bo_estado', 1)->orderBy('tx_descripcion', 'asc')->get(),
-            'estamentos' => Estamento::where('bo_estado', 1)->orderBy('tx_descripcion', 'asc')->get(),
+            'estamentos' => Estamento::where('bo_estado', 1)->where('cuenta_usuario', 1)->orderBy('tx_descripcion', 'asc')->get(),
             'sistemas' => GenSistema::where('sistema_local', 1)->orderBy('tx_descripcion', 'asc')->get(),
         ];
     }
