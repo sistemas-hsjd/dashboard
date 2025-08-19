e7570625611fe21a064e1be433c818fb08316fe6
Nelson Fredy <nelson.serrano@redsalud.gob.cl>
2025-08-19 08:51:52 -0400

    perfil 1 editar enlaces

--- resources/views/layouts/horizontal-menu.blade.php
+++ resources/views/layouts/horizontal-menu.blade.php
@@ -102,15 +102,7 @@
                                     <a href="#" class="dropdown-item" data-key="t-calendar"  data-bs-toggle="modal" data-bs-target="#modalCrearCuenta">Solicitar Cuenta</a>
                                 </div>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-pages" role="button">
-                                    <i data-feather="grid"></i><span data-key="t-apps">Mantenedores</span>
-                                    <div class="arrow-down"></div>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="topnav-pages">
-                                    <a href="#" class="dropdown-item" data-key="t-calendar"  data-bs-toggle="modal" data-bs-target="#modalEditEnlaces">Enlaces</a>
-                                </div>
-                            </li>
+                          
                         @endif
                     @else
                      <li class="nav-item dropdown">
@@ -123,6 +115,19 @@
                         </div>
                     </li>
                     @endif
+
+                    @if(Auth::user()->id_perfil==1)
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-pages" role="button">
+                                <i data-feather="grid"></i><span data-key="t-apps">Mantenedores</span>
+                                <div class="arrow-down"></div>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="topnav-pages">
+                                <a href="#" class="dropdown-item" data-key="t-calendar"  data-bs-toggle="modal" data-bs-target="#modalEditEnlaces">Enlaces</a>
+                            </div>
+                        </li>
+                    @endif
+
                     <li class="nav-item dropdown ">
                         <a class="nav-link dropdown-toggle arrow-none" href="#" role="button" data-bs-toggle="modal" data-bs-target=".bs-example-modal-xl">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layout"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line></svg><span data-key="t-horizontal">Soporte</span>
